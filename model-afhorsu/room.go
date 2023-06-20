aa539c6f349c0f950abd4ae7e7e8e615547e3bb5
shivam904455 <shivamrajpoot904455@gmail.com>
2023-06-20 11:35:44 +0530

    ste some flages

---
+++ model-afhorsu/room.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"time"
+
+	uuid "github.com/google/uuid"
+)
+
+type Room struct {
+	ID                 uuid.UUID `json:"id" gorm:"not null"`
+	HouseID            uuid.UUID `json:"house_id" binding:"required" gorm:"not null"`
+	FloorID            uuid.UUID `json:"foolr_id" binding:"required" gorm:"not null"`
+	Name               string    `json:"name" binding:"required" gorm:"not null"`
+	Description        string    `json:"description"`
+	Area               string    `json:"area"`
+	Occupancy          string    `json:"occuoancy"`
+	HeatingEnabled     string    `json:"heating_enabled"`
+	HeatingType        string    `json:"heating_type"`
+	CoolingEnabled     string    `json:"cooling_enabled"`
+	CoolingType        string    `json:"cooling_type"`
+	VantilationEnabled string    `json:"vantilation_enabled"`
+	VantilationType    string    `json:"vantilation_type"`
+	Teamperature       string    `json:"teamperature"`
+	Humidity           string    `json:"humidity"`
+	LightLavel         string    `json:"light_lavel"`
+	Co2Lavel           string    `json:"co2_lavel"`
+	CreatedAt          time.Time `json:"created_at" gorm:"not null"`
+	CreatedBy          string    `json:"created_by" binding"required" gorm:"not null"`
+	UpdatedAt         time.Time `json:"upadated_at"`
+	UpdatedBy          string    `json:"updated_by"`
+}
