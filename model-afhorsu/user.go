33bb68e150873f1ce2d40789c335d0816fb692c7
shivam904455 <shivamrajpoot904455@gmail.com>
2023-06-23 12:06:21 +0530

    Sensor Reading API created

--- model-afhorsu/user.go
+++ model-afhorsu/user.go
@@ -8,31 +8,31 @@ import (
 	"gorm.io/gorm"
 )
 
-type user struct {
+type User struct {
 	gorm.Model
 	ID                    uuid.UUID `json:"id" gorm:"primarykey"`
-	OrgId                 uuid.UUID `json: "org_id"`
+	OrgId                 uuid.UUID `json:"org_id"`
 	Name                  string    `json:"name" binding:"required" gorm:"unique;not null"`
 	Publisher             string    `json:"publisher"`
 	Email                 string    `json:"email" binding:"required" gorm:"unique;not null"`
 	Password              string    `json:"passwoed" binding:"required" gorm:"not null"`
-	PrimaryMobileNumber   int       `json:"primary_mobile_number" binding: "required" gorm:"not null"`
+	PrimaryMobileNumber   int       `json:"primary_mobile_number" binding:"required" gorm:"not null"`
 	SecondaryMobileNumber int       `json:"secondary_mobile_number"`
 	LandLineNumber        int       `json:"land_line_number"`
 	ActiveStatus          bool      `json:"active_status" binding:"required" gorm:"not null"`
-	Type                  string    `json:"type" gorm: "not null"`
+	Type                  string    `json:"type" gorm:"not null"`
 	AddressType           string    `json:"address_type"`
 	HouseNo               int       `json:"house_no"`
 	HouseName             string    `json:"house_name"`
 	LaneNumber            int       `json:"lane_number"`
 	LaneName              string    `json:"lane_name"`
 	Landmark              string    `json:"landmark"`
-	District              string    `json:"district" binding:"required" gorm: "mot null"`
+	District              string    `json:"district" binding:"required" gorm:"mot null"`
 	Post                  string    `json:"post"`
 	City                  string    `json:"city"`
 	VIllage               string    `json:"village"`
 	State                 string    `json:"state" binding:"required" gorm:"not null"`
-	nation                string    `json:"nation"binding:"required" gorm :"not null"`
+	Nation                string    `json:"nation" binding:"required" gorm:"not null"`
 	CreatedBy             string    `json:"created_by" binding:"required" gorm:"not null"`
 	CreatedAt             time.Time `json:"created_at" gorm:"not null"`
 	UpdatedBy             string    `json:"updated_by"`
